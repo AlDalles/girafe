37c6935bc51c734e67205305a6344ce2dc2088a8
Allen <allen@localhost>
2021-03-10 18:20:38 +0200

    girafe final

--- resources/views/pages/advert/showOne.blade.php
+++ resources/views/pages/advert/showOne.blade.php
@@ -3,35 +3,38 @@
     <link rel="stylesheet" href="{{asset('css/index.css')}}">
 @endpush
 @section('content')
-<div class="main">
-    <div class="main__ad">
-        <div class="main__ad__datatime">
-            <div class="main__ad__datatime__data">{{$advert->created_at->isoFormat('HH:mm')}}</div>
-            <div class="main__ad__datatime__time">{{$advert->created_at->isoFormat('DD/MM/YYYY')}}</div>
-        </div>
-        <div class="main__ad__title">
-            <a href="{{route('show',$advert)}}" class="main__ad__title__inner">{{$advert->title}}</a>
-            @can('delete',$advert)
-            <form action="{{route('destroy',$advert)}}" class="main__ad__title__form" method="post">@csrf
-                <input type="submit"  class="main__ad__title__delete" value="X">
-                @method('delete')
-            </form>
-
-            @endcan
-        </div>
-        <div class="main__ad__author">
-            <a href="{{route('searchByUser',$advert->user->id)}}" class="main__ad__author__title">{{$advert->user->name}}</a>
-            @can('update',$advert)<form action="{{route('edit',$advert)}}" class="main__ad__title__form">
-                <input type="submit"  class="main__ad__title__delete" value="edit">
-            </form>
-                @endcan
-        </div>
-        <div>
-            <div class="main__ad__image">
-                <img src="{{asset($advert->image_patch)}}" alt="Image Ad" class="main__ad__image__inner"><!--в src это путь к картинке-->
+    <div class="main">
+        <div class="main__ad">
+            <div class="main__ad__datatime">
+                <div class="main__ad__datatime__data">{{$advert->created_at->isoFormat('HH:mm')}}</div>
+                <div class="main__ad__datatime__time">{{$advert->created_at->isoFormat('DD/MM/YYYY')}}</div>
             </div>
+            <div class="main__ad__title">
+                <a href="{{route('show',$advert)}}" class="main__ad__title__inner">{{$advert->title}}</a>
+                @can('delete',$advert)
+                    <form action="/delete/{{$advert->id}}" class="main__ad__title__form" method="post">@csrf
+                        <input type="submit" class="main__ad__title__delete" value="X">
+                        @method('delete')
+                    </form>
 
-        </div>
+                @endcan
+            </div>
+            <div class="main__ad__author">
+                <a href="{{route('searchByUser',$advert->user->id)}}"
+                   class="main__ad__author__title">{{$advert->user->name}}</a>
+                @can('update',$advert)
+                    <form action="/edit/{{$advert->id}}" class="main__ad__title__form">
+                        <input type="submit" class="main__ad__title__delete" value="edit">
+                    </form>
+                @endcan
+            </div>
+            <div>
+                <div class="main__ad__image">
+                    <img src="{{asset($advert->image_patch)}}" alt="Image Ad" class="main__ad__image__inner">
+                    <!--в src это путь к картинке-->
+                </div>
+
+            </div>
         <div class="main__ad__description main__ad__description-item">
             {{$advert->description}}
         </div>
